54c4ef85ca332597f24e52e5f246de56cb94b092
Ivy Xu <ivyvyx116@gmail.com>
2022-07-15 20:12:18 -0700

    Added SoundManager with bgm implemented

---
+++ RolltheDice/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundUpdateEvent
+{
+    public int targetAudioSource;
+    public int targetAudioClip;
+
+    public SoundUpdateEvent(int _source, int _clip)
+    {
+        targetAudioSource = _source;
+        targetAudioClip = _clip;
+    }
+}
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+    public AudioClip[] bgm_clips;
+    public AudioClip[] sfx_clips;
+    public AudioSource[] audioSource;
+
+    void Start()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(this.gameObject);
+
+        PlayBGM(0); // start with main bgm
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+     // placeholder
+    }
+   
+    // play the corresponding bgm
+    public void PlayBGM(int clipIdx)
+    {
+        audioSource[0].clip = bgm_clips[clipIdx];
+        audioSource[0].loop = true;
+        audioSource[0].Play();
+    }
+    
+    // Play the plane takeoff sfx
+    public void PlayPlane()
+    {
+        audioSource[0].clip = bgm_clips[2];
+        audioSource[0].loop = false;
+        audioSource[0].Play();
+    }
+
+    public void PlaySFX(int sfxIdx)
+    {
+       
+    }
+}
